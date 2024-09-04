9e8aeb465ac2a2023c5116f1c0b3ec3027facbcf
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2024-09-04 09:36:14 +0500

    add links js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const elHamurgerIcon = document.querySelector('.hamburger-icon');
 const elNavbar = document.querySelector('.site-nav');
 const elCloseNavbar = document.querySelector('.site-nav__header-btn');
+const elFeatureLinks = document.querySelectorAll('.featurs__link');
+const elFeatureItems = document.querySelectorAll('.featurs__item');
 
 elHamurgerIcon.addEventListener('click', () => {
 	elNavbar.classList.toggle('active');
@@ -9,4 +11,18 @@ elHamurgerIcon.addEventListener('click', () => {
 
 elCloseNavbar.addEventListener('click', () => {
 	elNavbar.classList.toggle('active');
+});
+
+elFeatureLinks.forEach(elFeatureLink => {
+	elFeatureLink.addEventListener('click', e => {
+		e.preventDefault();
+
+		// ITEM ACTIVE REMOVE
+		elFeatureItems.forEach(elFeatureItem => {
+			elFeatureItem.classList.remove('active');
+		});
+
+		// ADD LINK ACTIVE
+		elFeatureLink.parentElement.classList.add('active');
+	});
 });
